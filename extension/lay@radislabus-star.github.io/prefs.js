4dfec54211d3e441a67293dc01c24042a9718d86
radislabus <radislabus@proton.me>
2026-07-06 11:25:22 +0300

    Bump lay to 0.2.100

--- extension/lay@radislabus-star.github.io/prefs.js
+++ extension/lay@radislabus-star.github.io/prefs.js
@@ -6,7 +6,7 @@ import Gtk from 'gi://Gtk';
 import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 const CONFIG_PATH = GLib.get_home_dir() + '/.config/lay/config.json';
-const APP_VERSION = '0.2.99';
+const APP_VERSION = '0.2.100';
 const APP_RELEASE_DATE = '2026-07-06';
 const APP_URL = 'https://github.com/radislabus-star/lay-public';
 const APP_ICON_NAME = 'input-keyboard-symbolic';
