7c047293d8235bbe5ea5245734b9fde28f25dad1
radislabus <radislabus@proton.me>
2026-07-10 23:26:59 +0300

    Separate transition signal scoring from admission

--- extension/lay@radislabus-star.github.io/settings.js
+++ extension/lay@radislabus-star.github.io/settings.js
@@ -4,7 +4,7 @@ import GLib from 'gi://GLib';
 import Gtk from 'gi://Gtk';
 
 const CONFIG_PATH = GLib.get_home_dir() + '/.config/lay/config.json';
-const APP_VERSION = '0.2.205';
+const APP_VERSION = '0.2.206';
 const APP_RELEASE_DATE = '2026-07-10';
 const APP_URL = 'https://github.com/radislabus-star/lay-public';
 const APP_ICON_NAME = 'input-keyboard-symbolic';
